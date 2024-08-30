0ee0b87bc7cdd40215889154b6475cd50ec079d0
lygold <levigoldfein@gmail.com>
2024-08-30 18:00:54 +0300

    Temporarily adjusted time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Nov 04, 2024 18:00:00").getTime();
+    const countDate = new Date("Nov 04, 2024 17:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
